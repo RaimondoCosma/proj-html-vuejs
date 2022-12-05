6757440b50909bc7be3df87eefb77abb7dda9b34
RaimondoCosma <raimondo.cosma@gmail.com>
2022-12-05 11:32:49 +0100

    Aggiunge logo

---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,58 @@
+<script>
+import AppLogo from "./AppLogo.vue";
+export default {
+  name: "AppHeader",
+  components: {
+    AppLogo,
+  },
+  data() {
+    return {
+      navbar: [
+        {
+          link: "Home",
+          active: false,
+        },
+        {
+          link: "Services",
+          active: false,
+        },
+        {
+          link: "About",
+          active: false,
+        },
+        {
+          link: "Videos",
+          active: false,
+        },
+        {
+          link: "Blog",
+          active: false,
+        },
+        {
+          link: "Store",
+          active: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <section class="container nav">
+      <AppLogo />
+    </section>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  height: 6.25rem;
+  background-color: var(--primary-color);
+  .nav {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/components/header/AppLogo.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "AppLogo",
+};
+</script>
+
+<template>
+  <div class="logo">
+    <a href="#"><img src="./../../assets/img/logo.png" alt="" /></a>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
