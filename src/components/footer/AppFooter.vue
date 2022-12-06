c4b9c5ce35934f04cd9cb57e537dbe4605df0bbf
RaimondoCosma <raimondo.cosma@gmail.com>
2022-12-07 00:56:05 +0100

    Aggiunge credits footer

--- src/components/footer/AppFooter.vue
+++ src/components/footer/AppFooter.vue
@@ -1,15 +1,18 @@
 <script>
 import AppPreFooter from "./pre-footer/AppPreFooter.vue";
+import AppCredits from "./credits-footer/AppCredits.vue";
 export default {
   name: "AppFooter",
   components: {
     AppPreFooter,
+    AppCredits,
   },
 };
 </script>
 
 <template>
   <AppPreFooter />
+  <AppCredits />
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/footer/credits-footer/AppCredits.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "AppCredits",
+};
+</script>
+
+<template>
+  <div class="credits">
+    <p>
+      <i class="fa-regular fa-copyright"></i> Copyright 2012 - 2020 | Avada
+      theme by <strong>ThemeFusion</strong> | All rights reserved | Powered by
+      <strong>WordPress</strong>
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.credits {
+  text-align: center;
+  background-color: var(--primary-color);
+  padding: 2.5rem 0;
+  border-top: 0.0625rem solid var(--secondary-color);
+  strong {
+    color: var(--lighter-color);
+  }
+}
+</style>
